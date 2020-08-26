ff85752a31fdb5cab755c1d9f11137c53c96fe98
usg <phperbar@163.com>
2020-08-26 08:58:18 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 import React, { useEffect, useState } from 'react'
+import {
+  G2,
+  Chart,
+  Tooltip,
+  Interval,
+  Coord
+} from "bizcharts";
 import ReactDOM from "react-dom";
-const Counter = () => {
-  const [count, setCount] = useState(0)
 
-  const handleAlertClick = () => {
-    setTimeout(() => {
-      alert(`Yout clicked me: ${count}`)
-    }, 3000)
-  }
-
-  useEffect(() => {
-      console.log(`Yout clicked ${count} times`);
-  })
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <button onClick={handleAlertClick}>Show alert</button>
-    </div>
-  )
-}
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Counter />, rootElement);
+
+
+const data = [
+  { name: 'London', 月份: 'Jan.', 月均降雨量: 18.9 },
+  { name: 'London', 月份: 'Feb.', 月均降雨量: 28.8 },
+  { name: 'London', 月份: 'Mar.', 月均降雨量: 39.3 },
+  { name: 'London', 月份: 'Apr.', 月均降雨量: 81.4 },
+  { name: 'London', 月份: 'May', 月均降雨量: 47 },
+  { name: 'London', 月份: 'Jun.', 月均降雨量: 20.3 },
+  { name: 'London', 月份: 'Jul.', 月均降雨量: 24 },
+  { name: 'London', 月份: 'Aug.', 月均降雨量: 35.6 },
+  { name: 'Berlin', 月份: 'Jan.', 月均降雨量: 12.4 },
+  { name: 'Berlin', 月份: 'Feb.', 月均降雨量: 23.2 },
+  { name: 'Berlin', 月份: 'Mar.', 月均降雨量: 34.5 },
+  { name: 'Berlin', 月份: 'Apr.', 月均降雨量: 99.7 },
+  { name: 'Berlin', 月份: 'May', 月均降雨量: 52.6 },
+  { name: 'Berlin', 月份: 'Jun.', 月均降雨量: 35.5 },
+  { name: 'Berlin', 月份: 'Jul.', 月均降雨量: 37.4 },
+  { name: 'Berlin', 月份: 'Aug.', 月均降雨量: 42.4 },
+];
+
+function Demo() {
+  return (
+    <Chart height={400} padding="auto" data={data} autoFit>
+      <Interval
+        adjust={[
+         {
+            type: 'stack',
+          },
+        ]}
+        color="name"
+        position="月份*月均降雨量"
+      />
+      <Tooltip shared />
+    </Chart>
+  );
+}
+
+ReactDOM.render(<Demo />, rootElement)
